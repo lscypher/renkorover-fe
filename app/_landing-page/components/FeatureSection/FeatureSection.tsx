739b9fb53591be4d44adcf66c76e0dfbfae298da
lscypher <168063054+lscypher@users.noreply.github.com>
2024-04-29 13:45:27 +0530

    feat: Basic Feature Page (#2)
    * feat: Basic Feature Page
    
    * feat: Added Feature Images

--- app/_landing-page/components/FeatureSection/FeatureSection.tsx
+++ app/_landing-page/components/FeatureSection/FeatureSection.tsx
@@ -1,19 +1,52 @@
-import Container from "@/components/layout/Container"
+import classNames from 'classnames';
+import Image from 'next/image';
 
-import styles from './FeatureSection.module.scss'
-type Props = {}
+import Container from '@/components/layout/Container';
+import Txt from '@/components/common/Txt';
 
-const FeatureSection = (props: Props) => {
+import { FEATURES, TOP_HEADING, TOP_SUB_HEADING } from './constant';
+
+import styles from './FeatureSection.module.scss';
+
+const FeatureSection = () => {
   return (
-    <Container className='section'>
-      <Container className={styles.left}>
-        Hello
+    <Container
+      component='section'
+      className={classNames('section', styles.container)}
+    >
+      <Container className={styles.top}>
+        <Txt color='dark' bold className={styles.topHeading}>
+          {TOP_HEADING}
+          <Txt color='dark' cursive className={styles.topCursive}>
+            AI-Powered
+          </Txt>
+          Workspace
+        </Txt>
+        <Txt color='dark' size='l' className={styles.topSubHeading}>
+          {TOP_SUB_HEADING}
+        </Txt>
       </Container>
-      <Container className={styles.right}>
-        World
+      <Container className={styles.bottom}>
+        <Container className={styles.featuresContainer}>
+          {FEATURES.map(({ heading, content, icon }, index) => {
+            return (
+              <Container className={styles.featureContainer} key={index}>
+                <Container className={styles.featureIcon}>
+                  <Image src={icon} alt={`${index}`} height={50} />
+                </Container>
+                <Txt color='dark' size='l' className={styles.featureHeading}>
+                  {heading}
+                </Txt>
+                <Txt color='dark' size='m' className={styles.featureContent}>
+                  {content}
+                </Txt>
+              </Container>
+            );
+          })}
+        </Container>
       </Container>
     </Container>
-  )
-}
+  );
+};
 
 export default FeatureSection;
---
+++ app/_landing-page/components/FeatureSection/constant.ts
@@ -0,0 +1,53 @@
+import Feature1 from '/public/landing_page/feature1.svg';
+import Feature2 from '/public/landing_page/feature2.svg';
+import Feature3 from '/public/landing_page/feature3.svg';
+import Feature4 from '/public/landing_page/feature4.svg';
+import Feature5 from '/public/landing_page/feature5.svg';
+import Feature6 from '/public/landing_page/feature6.svg';
+
+export const TOP_HEADING = `All Your Data Needs in a \n Single `;
+export const TOP_SUB_HEADING = `Hey there world traveler! It’s time for you globetrotters to 
+rejoice cos’ sending money abroad just got stupid simple :)`;
+export const FEATURES = [
+  {
+    id: '',
+    icon: Feature1,
+    heading: 'Automate Your Trades with Our Intelligent Bot',
+    content: `Looking for a smarter, faster, and more efficient way to trade?
+       Our trading bot is here to help.Our intelligent system will analyze the market.`,
+  },
+  {
+    id: '',
+    icon: Feature2,
+    heading: 'Real-Time Scalping',
+    content: `Execute rapid, real-time trades within the Nifty 50 options market, 
+    leveraging swift market movements for quick profits.`,
+  },
+  {
+    id: '',
+    icon: Feature3,
+    heading: 'Risk Management System',
+    content: `Implements robust risk management protocols to minimize potential 
+    losses, ensuring a balanced approach to trading.`,
+  },
+  {
+    id: '',
+    icon: Feature4,
+    heading: 'Dynamic Strategy Adaptation',
+    content: `Transfer your earnings to your local bank account at low rates or via ATM`,
+  },
+  {
+    id: '',
+    icon: Feature5,
+    heading: 'Customizable Parameters:',
+    content: `Pay the VAT authorities in the EU and UK with the 
+    GBP and EUR funds in your Payoneer account, for zero fees`,
+  },
+  {
+    id: '',
+    icon: Feature6,
+    heading: 'Connect with ecosystem',
+    content: `Leverage our network of integrated service 
+    providers to simplify your business, help you grow globall`,
+  },
+];
