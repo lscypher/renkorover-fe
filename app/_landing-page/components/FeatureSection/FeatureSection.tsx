ec29c37ff7a3cdd7a5fdf73df39cc53a94ef4a7f
lscypher <sahil@renkorover.com>
2024-04-29 11:55:33 +0530

    feat: Basic Feature Page

--- app/_landing-page/components/FeatureSection/FeatureSection.tsx
+++ app/_landing-page/components/FeatureSection/FeatureSection.tsx
@@ -1,19 +1,30 @@
-import Container from "@/components/layout/Container"
+import classNames from 'classnames';
 
-import styles from './FeatureSection.module.scss'
-type Props = {}
+import Container from '@/components/layout/Container';
+import Txt from '@/components/common/Txt';
 
-const FeatureSection = (props: Props) => {
+import { TOP_HEADING, TOP_SUB_HEADING } from './constant';
+
+import styles from './FeatureSection.module.scss';
+
+const FeatureSection = () => {
   return (
-    <Container className='section'>
-      <Container className={styles.left}>
-        Hello
+    <Container className={classNames('section', styles.container)}>
+      <Container className={styles.top}>
+        <Txt color='dark' bold className={styles.topHeading}>
+          {TOP_HEADING}
+          <Txt color='dark' cursive className={styles.topCursive}>
+            AI-Powered
+          </Txt>
+          Workspace
+        </Txt>
+        <Txt color='dark' size='l' className={styles.topSubHeading}>
+          {TOP_SUB_HEADING}
+        </Txt>
       </Container>
-      <Container className={styles.right}>
-        World
-      </Container>
+      <Container className={styles.bottom}>World</Container>
     </Container>
-  )
-}
+  );
+};
 
 export default FeatureSection;
---
+++ app/_landing-page/components/FeatureSection/constant.ts
@@ -0,0 +1,40 @@
+export const TOP_HEADING = `All Your Data Needs in a \n Single `;
+export const TOP_SUB_HEADING = `Hey there world traveler! It’s time for you globetrotters to 
+rejoice cos’ sending money abroad just got stupid simple :)`;
+export const FEATURES = [
+  {
+    id: '',
+    heading: 'Automate Your Trades with Our Intelligent Bot',
+    content: `Looking for a smarter, faster, and more efficient way to trade?
+       Our trading bot is here to help.Our intelligent system will analyze the market.`,
+  },
+  {
+    id: '',
+    heading: 'Real-Time Scalping',
+    content: `Execute rapid, real-time trades within the Nifty 50 options market, 
+    leveraging swift market movements for quick profits.`,
+  },
+  {
+    id: '',
+    heading: 'Risk Management System',
+    content: `Implements robust risk management protocols to minimize potential 
+    losses, ensuring a balanced approach to trading.`,
+  },
+  {
+    id: '',
+    heading: 'Dynamic Strategy Adaptation',
+    content: `Transfer your earnings to your local bank account at low rates or via ATM`,
+  },
+  {
+    id: '',
+    heading: 'Customizable Parameters:',
+    content: `Pay the VAT authorities in the EU and UK with the 
+    GBP and EUR funds in your Payoneer account, for zero fees`,
+  },
+  {
+    id: '',
+    heading: 'Connect with ecosystem',
+    content: `Leverage our network of integrated service 
+    providers to simplify your business, help you grow globall`,
+  },
+];
